57eb086849146e3cb74b6339f89124276b3cf3ad
Çankırı İl Sağlık Müdürlüğü <36158042+Cankirism@users.noreply.github.com>
2021-03-10 10:08:10 +0300

    Update mailsender.py

--- mailsender.py
+++ mailsender.py
@@ -3,28 +3,26 @@ from smtplib import SMTP
 from email.mime.text import MIMEText
 
 
-class MailSender:
+def send_mail(self, message):
+    #  İçerik
+    subject = "SBA Botu Alarm"
+    content = message
 
-    def send_mail(self, message):
-        #  İçerik
-        subject = "SBA Botu Alarm"
-        content = message
+    #  Account
+    from_ = "gönderen eposta"
+    to_ = "alıcı eposta"
+    passwd = "gönderen eposta pass"
 
-        #  Account
-        from_ = "gönderen eposta"
-        to_ = "alıcı eposta"
-        passwd = "gönderen eposta pass"
-
-        # Mail gönder
-        mail = MIMEText(content, "utf-8")
-        mail["From"] = from_
-        mail["To"] = to_
-        mail["Subject"] = subject
-        mail = mail.as_string()
-        smtp = SMTP("smtp.gmail.com", 587)
-        smtp.starttls()
-        smtp.login(from_, passwd)
-        smtp.sendmail(from_, to_, mail)
+    # Mail gönder
+    mail = MIMEText(content, "utf-8")
+    mail["From"] = from_
+    mail["To"] = to_
+    mail["Subject"] = subject
+    mail = mail.as_string()
+    smtp = SMTP("smtp.gmail.com", 587)
+    smtp.starttls()
+    smtp.login(from_, passwd)
+    smtp.sendmail(from_, to_, mail)
 
 
 
