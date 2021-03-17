868365384221996a03b36f769fd846311568a856
cankirism <csmbilsis@saglik.gov.tr>
2021-03-17 10:14:49 +0300

    sba kısmı düzenlendi

--- mailsender.py
+++ mailsender.py
@@ -3,26 +3,28 @@ from smtplib import SMTP
 from email.mime.text import MIMEText
 
 
-def send_mail(self, message):
-    #  İçerik
-    subject = "SBA Botu Alarm"
-    content = message
+class MailSender:
 
-    #  Account
-    from_ = "gönderen eposta"
-    to_ = "alıcı eposta"
-    passwd = "gönderen eposta pass"
+    def send_mail(message):
+        #  İçerik
+        subject = "SBA Botu Alarm"
+        content = message
 
-    # Mail gönder
-    mail = MIMEText(content, "utf-8")
-    mail["From"] = from_
-    mail["To"] = to_
-    mail["Subject"] = subject
-    mail = mail.as_string()
-    smtp = SMTP("smtp.gmail.com", 587)
-    smtp.starttls()
-    smtp.login(from_, passwd)
-    smtp.sendmail(from_, to_, mail)
+        #  Account
+        from_ = "gönderen eposta"
+        to_ = "alıcı eposta"
+        passwd = "gönderen eposta pass"
+
+        # Mail gönder
+        mail = MIMEText(content, "utf-8")
+        mail["From"] = from_
+        mail["To"] = to_
+        mail["Subject"] = subject
+        mail = mail.as_string()
+        smtp = SMTP("smtp.gmail.com", 587)
+        smtp.starttls()
+        smtp.login(from_, passwd)
+        smtp.sendmail(from_, to_, mail)
 
 
 
